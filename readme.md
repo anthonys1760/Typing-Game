5ee06d278e7c13f509132cc19b7421eea63c51e9
Anthony <anthony.smith1760@gmail.com>
2020-04-30 12:56:52 -0400

    Finished Game

---
+++ readme.md
@@ -0,0 +1,27 @@
+## Speed Typer Typing Game
+
+Game to beat the clock by typing random words
+
+![title-pic](typingimage.png)
+
+ ## Project Specifications
+
+- Create game UI including a difficuly setting
+- Generate random word and place in DOM
+- Score increase after word is typed
+- Implement timer
+- Add certain amount of time after word is typed based on difficulty
+- Store difficulty setting in local storage
+ 
+## Steps to execute this Typing game:
+- Download the entire code 
+- Open up the index.html.
+- [View Live Site](https://anthonys1760.github.io/Simple-Calculator/)
+
+## Technologies used: 
+- HTML
+- CSS [style/Presentation]
+- Javascript [Logic/Working of game]
+
+
+
--- script.js
+++ script.js
@@ -42,6 +42,20 @@ let score = 0;
 //Init time
 let time = 10;
 
+//Set difficulty to value inls or medium
+let difficulty = 
+  localStorage.getItem('difficulty') !== null 
+    ? localStorage.getItem('difficulty') 
+    : 'medium';
+
+
+//Set difficulty select value
+difficultySelect.value = 
+  localStorage.getItem('difficulty') !== null 
+    ? localStorage.getItem('difficulty') 
+    : 'medium';
+
+
 //Focus on text on start
 text.focus();
 
@@ -90,10 +104,13 @@ function gameOver() {
 
 addWordToDom();
 
-//Event Listener
+//Event listeners
 
+
+//Typing
 text.addEventListener('input', e => {
     const insertedText = e.target.value;
+
     if (insertedText === randomWord) {
         addWordToDom();
         updateScore();
@@ -101,9 +118,24 @@ text.addEventListener('input', e => {
         //Clear
         e.target.value = '';
 
-        time += 5;
+        if (difficulty === 'hard') {
+            time += 1;
+        } else if (difficulty === 'medium') {
+            time += 3;
+        } else {
+            time += 5;
+        }
 
         updateTime();
 
     }
+});
+
+//Settings btn click
+settingsBtn.addEventListener('click', () => settings.classList.toggle('hide'));
+
+// Settings select
+settingsForm.addEventListener('change', e => {
+    difficulty = e.target.value;
+    localStorage.setItem('difficulty', difficulty);
 });
